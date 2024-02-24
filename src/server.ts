4842d59722b0b9b70bc3a0d3a88ea4efe55f68a5
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-02-24 17:49:30 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -4,3 +4,22 @@
 // console.log(currentTime);
 // const person: string = "Martin";
 // const count: number = 100;
+
+// Ushbu architectural va Design patternlarni yaxshi bilib olsangiz, programming
+// language dan boshqa programming language ga o`tish qiyinchilik tug`dirmaydi.
+// Architectural pattern:
+// 1. Backend: MVC (model view controller), DI (Dependency  Injection),
+// 2. Frontend: MVP (Modal view presenter)
+//  Design patterns: Middleware, Decorator
+
+// Architectural pattern, bu backend yoki frontend ni qurib beruvchi suyagi bo`ladi. Backend da ma`lumotlar oqimini
+// tartibga soluvchi vosita, arxitekturasi hisoblanadi.
+// Design pattern: bu backend da ma`lum bir bo`laklarni strukturasini yechishda xizmat qiladigan
+// vositalar. Design pattern hisoblanadi.
+
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:", process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
