6786aee778e9e68db861f768523e94f5bfeb8309
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-02-28 01:07:02 +0900

    Feat: member schema

--- src/train copy.ts
+++ src/train copy.ts
@@ -8,25 +8,43 @@ console.log("hello world");
 
 //
 
-function getHighestIndex(arr: number[]): number {
-  if (arr.length === 0) {
-    // Agar array bo'sh bo'lsa -1 qaytaramiz (masalan, array yo'q)
-    return -1;
-  }
+// function getHighestIndex(arr: number[]): number {
+//   if (arr.length === 0) {
+//     // Agar array bo'sh bo'lsa -1 qaytaramiz (masalan, array yo'q)
+//     return -1;
+//   }
 
-  let highestIndex = 0;
-  let highestValue = arr[0];
+//   let highestIndex = 0;
+//   let highestValue = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > highestValue) {
-      highestValue = arr[i];
-      highestIndex = i;
-    }
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] > highestValue) {
+//       highestValue = arr[i];
+//       highestIndex = i;
+//     }
+//   }
 
-  return highestIndex;
-}
+//   return highestIndex;
+// }
 
 // Test qilish
-const result = getHighestIndex([5, 1, 12, 21, 8]);
-console.log(result); // Natija: 1
+// const result = getHighestIndex([5, 1, 12, 21, 8]);
+// console.log(result); // Natija: 1
+
+console.log("TASK-H");
+
+// Task H:
+
+// Shunday function tuzing, u string qabul qilib teskari qilib return qilsin
+// MASALAN: gerReverse("hello") return qiladi "olleh"
+
+// function gerReverse(str: string): string {
+//   let reversedStr = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversedStr += str[i];
+//   }
+//   return reversedStr;
+// }
+
+// const resultGer = gerReverse("Hello World!");
+// console.log(resultGer); // natijasi: !dlroW olleH
