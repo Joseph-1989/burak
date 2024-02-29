3bc94ca6528e3d1e142c3955194cfcf1210e19ac
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-02-29 14:28:48 +0900

    feat: create restaurant member related router endpoints

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -3,10 +3,20 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
+// RESTAURANT:
 routerAdmin.get("/", restaurantController.goHome);
 
-routerAdmin.get("/login", restaurantController.getLogin);
+routerAdmin
+  .get("/login", restaurantController.getLogin)
+  .post("/login", restaurantController.processLogin);
 
-routerAdmin.get("/signup", restaurantController.getSignup);
+// routerAdmin.post("/login/process", restaurantController.processLogin);
+// routerAdmin.post("/login", restaurantController.processLogin);
 
+routerAdmin
+  .get("/signup", restaurantController.getSignup)
+  .post("/signup", restaurantController.processSignup);
+
+//   PRODUCTS:
+// USER:
 export default routerAdmin;
