cd52de67fc42b7c4fe8e88e5758baedfccadad69
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-03-01 15:53:29 +0900

    feat: develop signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -1,5 +1,42 @@
-class MemberService {
-  constructor() {}
-}
+import { MemberType } from "../libs/enums/member.enum";
+import Errors, { HttpCode, Message } from "../libs/Error";
+import { Member, MemberInput } from "../libs/types/member";
+import MemberModel from "../schema/Member.model";
 
+class MemberService {
+  private readonly memberModel;
+
+  constructor() {
+    this.memberModel = MemberModel;
+  }
+
+  public async processSignup(input: MemberInput): Promise<Member> {
+    const exist = await this.memberModel
+      .findOne({
+        memberType: MemberType.RESTAURANT,
+      })
+      .exec(); // bu .exec(); methodini qo`ymasa ham, undan oldin ishlatilayotgan mongoose dagi method ishlayveradi.
+    // buning vasifasi, undan oldingi methodga qo`shimcha holatda boshqa methodlar qo`shilib kelmasligini bildiradi.
+    if (exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    console.log("exist:", exist);
+    try {
+      const result = await this.memberModel.create(input);
+      // const tempResult = new this.memberModel(input);
+      // const result = await tempResult.save();
+      console.log("This info is located in Member.service module!");
+      result.memberPassword = "";
+      return result;
+    } catch (err) {
+      throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+    }
+  }
+}
 export default MemberService;
+
+// static method nima
+// public async processSignup(): Promise<void> {
+//   console.log(
+//     "Processing sign up passed here: this info is located in Member.service module!"
+//   );
+// }
+// BIZ DOIM METHODLARNI ASYNC KO'RINISHDA YOZAMIZ, CHUNKI DATABASEDAN KO'P MUROJAATLAR BO'LASI.
