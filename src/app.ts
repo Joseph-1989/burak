45ec705b093d5dbb40d33cf8f523005eefcaa1b6
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-03-10 07:21:37 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,14 @@ import router from "./router"; //Our custom Express.js router
 import routerAdmin from "./route-admin"; //Router for admin functions (login, logout)
 import morgan from "morgan"; //Logging middleware for Express 4
 import { MORGAN_FORMAT } from "./libs/config"; //Config variable holding the logging format
+import session from "express-session"; //Simple session middleware for node.js
+import ConnectMongoDB from "connect-mongodb-session"; //Store sessions in MongoDB using connect-mongodb-session
+
+const MongoStore = ConnectMongoDB(session); //Connect MongoDB session store to Express Session
+const store = new MongoStore({
+  uri: String(process.env.MONGODB_URI), // || "mongodb://localhost/gallery",
+  collection: "sessions",
+});
 
 // #1-ENTRENCE
 const app = express();
@@ -14,6 +22,20 @@ app.use(express.json()); //for parsing application/json
 app.use(morgan(MORGAN_FORMAT)); //log request to console
 
 // #2-SESSIONS
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET), //"This is a secret",
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3 HOURS
+    },
+    store: store,
+    // Boilerplate options, see:
+    // * https://www.npmjs.com/package/express-session#resave
+    // * https://www.npmjs.com/package/express-session#saveuninitialized
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 // #3-VIEWS
 app.set("views", path.join(__dirname, "views")); // set views folder
