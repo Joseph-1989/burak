58793e12f1a7aa5a404b24a3ad7556a0ccd6d235
Joseph-1989 <akmal.khasanov1989@gmail.com>
2024-03-18 14:45:21 +0900

    feat: validateSignupForm functionni yozish

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,5 +1,33 @@
 console.log("Signup frontend javascript file");
 
+$(function () {
+  const fileTarget = $(".file-box .upload-hidden  ");
+  let filename;
+
+  fileTarget.on("change", function () {
+    if (window.FileReader) {
+      const uploadFile = $(this)[0].files[0]; // get the file
+      console.log("uploadFile     : ", uploadFile);
+      const fileType = uploadFile.type["type"]; // image, video etc
+      const validImageType = ["image/jpeg", "image/jpg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert(
+          "Invalid File Type! Please select an Image only jpeg, jpg, and png format."
+        );
+      } else {
+        if (uploadFile) {
+          console.log(URL.createObjectURL(uploadFile));
+          $(".upload-img-frame")
+            .attr("src", URL.createObjectURL(uploadFile))
+            .addClass("success");
+        }
+        filename = $(this)[0].files[0].name;
+      }
+      $(this).siblings(".upload-name").val(filename);
+    }
+  });
+});
+
 function validateSignupForm() {
   const memberNick = $(".member-nick").val(); //
   const memberPhone = $(".member-phone").val();
@@ -17,6 +45,14 @@ function validateSignupForm() {
   }
   if (memberPassword !== confirmPassword) {
     alert("Passwords do not match");
+    return false;
+  }
+
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
+    : null;
+  if (!memberImage) {
+    alert("Profile image is required.");
     return false;
   }
 }
